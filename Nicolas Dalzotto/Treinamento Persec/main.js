27f29036e9f52b90020704b13236da3044982dcb
aris402 <aris4455@hotmail.com>
2022-12-17 12:31:01 -0300

    Finishing Persec and Despertar da Elegancia pages

--- Nicolas Dalzotto/Treinamento Persec/main.js
+++ Nicolas Dalzotto/Treinamento Persec/main.js
@@ -1,5 +1,5 @@
 const button = document.querySelector("#second-button");
 
 button.addEventListener('click', () =>{
-    window.scroll({top: window.top, behavior: "smooth"})
+    window.scroll({bottom: window.bottom, behavior: "smooth"})
 })
